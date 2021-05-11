58a6ba5a06f8b3b2d5ebc2fee252dbc4956d3a37
Dogan Cem GUREL <dcgurel@gmail.com>
2021-05-11 22:26:02 +0100

    initial commit

---
+++ endpoint.py
@@ -0,0 +1,40 @@
+import helper, requests, json, secrets, constants
+from api import Api
+
+class Endpoint(Api):
+    def __init__(self, action, includes = []):
+        Api.__init__(self)
+        self.includes = includes
+        self.action = action
+        self.includes = includes
+
+    def query(self):
+        self.latest_query = self.action
+        with requests.Session() as r:
+            # try to call api with no params // todo => add params
+            try:
+                response = r.get(constants.API_URL + self.action, params=[('api_token', secrets.API_KEY),('page', "1"),('include', ",".join(self.includes))], timeout=constants.DEFAULT_TIMEOUT)
+                self.query_amt += 1
+            except Exception as e:
+                helper.write_log(f"Latest API query of : {self.latest_query} failed with {e}")
+                self.error = e
+                return False
+
+            raw_data = json.loads(response.text)
+            # find last page if there's pagination in the result
+            last_page = self.get_total_pages(raw_data)
+
+            # paginate if last_page is bigger than 1
+            if(int(last_page) > 1):
+                # return paginated result ( merged list of dictionaries )
+                self.paginator(raw_data, last_page)
+            else:
+                # return json object's data key ( where the meat really is )
+                try:
+                    self.data = raw_data['data']
+                except Exception as e:
+                    helper.write_log(f"Latest API query of : {self.latest_query} failed with {e}")
+                    self.error = e
+                    return False
+                else:
+                    return True
