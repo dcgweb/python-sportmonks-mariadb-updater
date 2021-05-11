ec01659463967248c479ec6a9b6959846c7c4503
Dogan Cem GUREL <dcgurel@gmail.com>
2021-05-11 22:34:59 +0100

    moved endpoint and query logic

--- api.py
+++ api.py
@@ -5,43 +5,11 @@ class Api:
         return self
 
     def __init__(self):
-        self.latest_query = None
-        self.query_amt = 0
         self.data = []
         self.error = None
+        self.query_amt = 0
+        self.latest_query = None
         print('Successfully initialized API Class')
-
-    def query(self, action):
-        self.action = action
-        self.latest_query = self.action
-        with requests.Session() as r:
-            # try to call api with no params // todo => add params
-            try:
-                response = r.get(secrets.API_URL + self.action, params=[('api_token', secrets.API_KEY), ('page', "1")], timeout=constants.DEFAULT_TIMEOUT)
-                self.query_amt += 1
-            except Exception as e:
-                helper.write_log(f"Latest API query of : {self.latest_query} failed with {e}")
-                self.error = e
-                return False
-
-            raw_data = json.loads(response.text)
-            # find last page if there's pagination in the result
-            last_page = self.get_total_pages(raw_data)
-
-            # paginate if last_page is bigger than 1
-            if(int(last_page) > 1):
-                # return paginated result ( merged list of dictionaries )
-                self.paginator(raw_data, last_page)
-            else:
-                # return json object's data key ( where the meat really is )
-                try:
-                    self.data = raw_data['data']
-                except Exception as e:
-                    helper.write_log(f"Latest API query of : {self.latest_query} failed with {e}")
-                    self.error = e
-                    return False
-                else:
-                    return True
 
     def get_total_pages(self, data):
         try:
