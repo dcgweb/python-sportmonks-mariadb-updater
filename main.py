aa256dde8097c7c8a4c7d126594f25b920d35524
Dogan Cem GUREL <dcgurel@gmail.com>
2021-05-28 03:52:51 +0100

    reoganizing code for reusability

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import data as formatter
 from logging.handlers import RotatingFileHandler
-import db, endpoint, time, helper, logging, constants
+import db, endpoint, helper, logging, constants
 
 
 class App:
@@ -10,31 +10,58 @@ class App:
         self.file_handler = RotatingFileHandler(constants.APP_LOG_FILENAME, maxBytes=constants.LOG_MAX_BYTES, backupCount=constants.LOG_BACKUP_COUNT)
         self.file_handler.setFormatter(logging.Formatter(constants.LOG_FORMAT))
         self.logger.addHandler(self.file_handler)
+        self.current_ep = None
+        self.current_db_tbl = None
 
-    def go(self, api_endpoint = None, api_includes = []):
-        if(api_endpoint == None):
-            self.logger.warning(f"api_endpoint parameter must not be empty")
+    def insert(self, unformatted_data = None):
+        if(unformatted_data == None):
+            self.logger.info("API response is empty")
             return False
-        start = time.time()
-        self.api_endpoint = api_endpoint
-        self.api_includes = api_includes
-        #init endpoint class with context manager
+
+        with formatter.Data() as f_d:
+            f_d.reshape(unformatted_data, self.current_ep)
+            with db.Db() as database:
+                try:
+                    helper.db_summary(database.insert_or_update(self.current_db_tbl, f_d.reshaped_data), self.current_ep, self.current_db_tbl)
+                except Exception as e:
+                    self.logger.error(e)
+                    return False
+                else:
+                    self.logger.info(f"{getattr(self, 'current_ep')} -> {getattr(self, 'current_db_tbl')}")
+                    return True
+
+    def extra_insert(self, unformatted_data = None, ep = None, db_tbl = None):
+        # This part is hacky but I cannot think of
+        # a better way to update more than one tables
+        # with one api query since livescores query result
+        # also should contain lineups data. So here we go!
+
+        # Update current objects props
+        self.current_ep = ep
+        self.current_db_tbl = db_tbl
+
+        # With these object props changed, it will
+        # now insert the extra data into other db tables
+        self.insert(unformatted_data)
+
+    def run(self, api_endpoint = None, api_includes = [], db_tbl = None):
+        if(api_endpoint == None or db_tbl == None):
+            return False
+
+        # Set initializing values of the app object
+        self.current_ep = api_endpoint
+        self.current_db_tbl = db_tbl
         with endpoint.Endpoint() as ep:
-            ep.query(self.api_endpoint, self.api_includes)
-            # If there are no records returned from API
-            if(len(ep.data) == 0):
-                logging.warning('Api query returned 0 results')
+            ep.query(self.current_ep, api_includes)
+            if not ep.error == None:
                 return False
 
-            with formatter.Data() as f_d:
-                f_d.reshape(ep.data, self.api_endpoint)
-                with db.Db() as database:
-                    affected_rows = database.insert_or_update('fixtures', f_d.reshaped_data)
-                    helper.db_summary(affected_rows)
-
-        logging.info(f"Took {str(round(time.time() - start,3))} seconds")
+            self.insert(ep.data)
+            if(api_endpoint == 'livescores'):
+                self.extra_insert(ep.data, 'lineups', 'lineups')
 
 
 if __name__ == "__main__":
     gololdu = App()
-    gololdu.go('livescores', ["goals", "cards", "stats", "tvstations", "comments"])
+    gololdu.run('livescores', ["goals", "cards", "stats", "tvstations", "comments", "lineup"], 'fixtures')
+    #gololdu.run('countries', [], 'countries')
