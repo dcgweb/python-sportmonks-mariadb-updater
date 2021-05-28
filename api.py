48a3d39b5bf3c570ef75528b7a4e32a1fd1fffd4
Dogan Cem GUREL <dcgurel@gmail.com>
2021-05-28 03:51:07 +0100

    moved pagination login into endpoint

--- api.py
+++ api.py
@@ -15,36 +15,6 @@ class Api:
         self.file_handler.setFormatter(logging.Formatter(constants.LOG_FORMAT))
         self.logger.addHandler(self.file_handler)
 
-    def get_total_pages(self, data):
-        try:
-            total_pages = data['meta']['pagination']['total_pages']
-        except:
-            self.logger.warning(f"Total pages count could not be determined, presuming it to be 1")
-            total_pages = 1
-        finally:
-            self.logger.debug(f"{str(total_pages)} in total")
-            return total_pages
-
-    def paginator(self, data, last_page):
-        merged_data = []
-        data_list = [].append(data)
-        for page in range(2, int(last_page) + 1):
-            with requests.Session() as r:
-                try:
-                    response = r.get(secrets.API_URL + self.action, params=[('api_token', secrets.API_KEY), ('page', str(page))], timeout=constants.DEFAULT_TIMEOUT)
-                    self.query_amt += 1
-                except Exception as e:
-                    self.logger.error(f"Latest API query of : {str(self.latest_query)} failed with {str(e)}")
-                    self.error = e
-                    return False
-                else:
-                    data_list.append(json.loads(response.text))
-            for f_data in data_list:
-                for c_data in f_data['data']:
-                    merged_data.append(c_data)
-            self.data = merged_data
-            return True
-
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.logger.debug(f"Total Queries : {self.query_amt}")
         if(self.error):
--- endpoint.py
+++ endpoint.py
@@ -4,9 +4,6 @@ import requests, json, secrets, constants, logging
 
 class Endpoint(Api):
 
-    def __enter__(self):
-        return self
-
     def __init__(self):
         super().__init__()
         self.logger = logging.getLogger(__name__)
@@ -14,15 +11,44 @@ class Endpoint(Api):
         self.file_handler = RotatingFileHandler(constants.EP_LOG_FILENAME, maxBytes=constants.LOG_MAX_BYTES, backupCount=constants.LOG_BACKUP_COUNT)
         self.file_handler.setFormatter(logging.Formatter(constants.LOG_FORMAT))
         self.logger.addHandler(self.file_handler)
-
-    def query(self, action, includes = []):
         self.data = []
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.data = []
+        self.action = None
+        self.includes = []
+        self.latest_query = None
+        self.logger.debug(f"Total Queries : {self.query_amt}")
+
+    def get_total_pages(self, data):
+        try:
+            total_pages = data['meta']['pagination']['total_pages']
+        except:
+            self.logger.warning(f"Total pages count could not be determined, presuming it to be 1")
+            return 1
+        else:
+            self.logger.info(f"{str(total_pages)} in total")
+            return total_pages
+
+    def paginator(self, last_page = None):
+        if(last_page == None):
+            return False
+
+        for page in range(2, int(last_page) + 1):
+            print(f'Paginating on {self.action}:{self.includes}:P{page}')
+            self.query(self.action, self.includes, page)
+
+    def query(self, action, includes = [], page = 1):
         self.action = action
         self.includes = includes
         self.latest_query = self.action
         with requests.Session() as r:
             try:
-                response = r.get(constants.API_URL + self.action, params=[('api_token', secrets.API_KEY),('page', "1"),('include', ",".join(self.includes))], timeout=constants.DEFAULT_TIMEOUT)
+                response = r.get(constants.API_URL + self.action, params=[('api_token', secrets.API_KEY), ('page', str(page)), ('include', ",".join(self.includes))], timeout=constants.DEFAULT_TIMEOUT)
+                #print(f"{constants.API_URL}{self.action} 'include'={self.includes} 'page'={str(page)}")
                 self.query_amt += 1
             except Exception as e:
                 self.logger.error(f"API query : {self.latest_query} failed with {e}")
@@ -32,28 +58,21 @@ class Endpoint(Api):
                 self.logger.debug(f"Response = {str(r)}")
                 raw_data = json.loads(response.text)
 
-                # find last page if there's pagination in the result
-                last_page = self.get_total_pages(raw_data)
+                # Check if it's already being paginated
+                if(page == 1):
+                    # find last page if there's pagination in the result
+                    last_page = self.get_total_pages(raw_data)
 
-                # paginate if last_page is bigger than 1
-                if(int(last_page) > 1):
-                    # return paginated result ( merged list of dictionaries )
-                    self.paginator(raw_data, last_page)
-                else:
-                    if(isinstance(raw_data, dict) == False):
-                        return False
-                    if('data' not in raw_data):
-                        return False
+                    # paginate if last_page is bigger than 1
+                    if(int(last_page) > 1):
+                        # paginate the result ( with extra api queries )
+                        self.paginator(last_page)
 
+            if(isinstance(raw_data, dict) == False):
+                return False
+            if('data' not in raw_data):
+                return False
+            if(len(raw_data['data']) == 0):
+                return False
 
-
-                self.data = raw_data['data']
-
-
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.data = []
-        self.action = None
-        self.includes = []
-        self.latest_query = None
-        self.logger.debug(f"Total Queries : {self.query_amt}")
+            self.data.extend(raw_data['data'])
