b37ecd5b7eeffd56262843c2c7b972a0d50dacde
Dogan Cem GUREL <dcgurel@gmail.com>
2021-06-09 00:17:53 +0100

    removed unnecessary constants

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 # Debug disables the logging to be printed on screen, writes all logs to a file
-DEBUG = True
+DEBUG = False
 
 # Loggging configuration
 LOG_LEVEL = 'WARNING'
@@ -21,12 +21,7 @@ LOG_FORMAT = '%(asctime)s:%(name)s:%(levelname)s:%(funcName)s:%(message)s'
 DEFAULT_SLEEP_INTERVAL = 30
 # Defines timeouts for every api query and etc.
 DEFAULT_TIMEOUT = (15, 30)
-TWITTER_BLOG_POST_CATEGORY = 3 # this is purely required for my specific configuration, can be overlooked
-
-
-SECOND_API_URL = "https://www.thesportsdb.com/api/v1/json/"
-TWITTER_API_URL = ""
-
+TWITTER_BLOG_POST_CATEGORY = 3 # this is purely required for my specific configuration, can be ignored
 API_URL = 'https://soccer.sportmonks.com/api/v2.0/'  # URL for API endpoint
 
 ACCOUNT_LIST = [
@@ -57,9 +52,4 @@ LEAGUE_NAMES_LIST = [
     'Super Lig'
 ]
 
-
-SELECTED_LEAGUES = [2,5,8,72,82,208,301,384,462,564,573,600]
 ACTIVE_COUNTRIES = [11, 17, 20, 32, 38, 41, 47, 251, 404, 462, 556]
-API_MULTI_QUERY_LIMIT = 25
-
-THIS_YEAR_00_TS = 1609459200 # 2021-01-01- 00:00:00
